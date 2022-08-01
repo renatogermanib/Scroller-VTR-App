4bedc37fb243c1d041baa97815e3d5a5e58a2180
Viruta <renatogermanib@gmail.com>
2022-08-01 14:36:24 -0400

    all functions complete

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from tkinter import Button, Tk, Frame, Entry, END
 #config ventana:
 ventana = Tk()
-ventana.geometry('240x280')
-ventana.config(bg='red')
+ventana.geometry('186x253')
+ventana.config(bg='dark slate gray')
 ventana.iconbitmap(bitmap='icono.ico')
 ventana.resizable(0,0)
 ventana.title('análisis')
@@ -21,58 +21,47 @@ class HoverButton(Button):
 		self['background'] = self.defaultBackground
 
 i=0
-'''
-def obtener(dato):
-	global i
-	i+=1
-	Resultado.insert(i, dato)
-'''
-def operacion(): #función relacionada al boton =
+
+def scroll(): #función relacionada al boton =
 	global i
 	i += 14
 	Resultado.delete(0, END)
 	Resultado.insert(0,i)
-	'''
-	i = 0
-	#ecuacion = Resultado.get()
-	if i == 0: #si se ingresó un dato
-		try:
-			print('soy la función dentro del try')
-			#result = str(eval(ecuacion)) #eval transforma strings en operaciones matemáticas
-			result = i + 14
-			Resultado.delete(0, END)
-			Resultado.insert(0,"hola")
-			longitud = len(result)
-			i = longitud
-			
-		except:
-			result = 'ERROR'
-			Resultado.delete(0,END)
-			Resultado.insert(0,result)
-	else:
-		pass
-	'''
-'''
-def eliminar():
+
+def rollback():
 	global i
-	Resultado.delete(i, END)
-	i=0
-'''
-def limpiar():
+	if i != 0:
+		i -= 14
+		Resultado.delete(0, END)
+		Resultado.insert(0,i)
+	else:
+		i = 0
+		Resultado.delete(0, END)
+		Resultado.insert(0,i)
+
+def reset():
+	global i
 	Resultado.delete(0, END)
 	i=0
+	Resultado.insert(0,i)
 
-frame = Frame(ventana, bg='gray', relief='raised')
-frame.grid(column=0, row=0, padx=1, pady=3, ipadx=1, ipady=1)
+'''
+frame = Frame(ventana, bg='dark slate gray', relief='raised', width=500, height=500)
+frame.grid()
+'''
 
-scroll = HoverButton(frame, text= "SCROLL ONE PAGE", height=2, width=18,font= ('Bahnschrift',13), borderwidth=2,  relief = "raised", activebackground="dark goldenrod", bg='gold',  anchor="center",command=lambda: operacion())  
-scroll.grid(column =3, row=1, pady=2,padx=2)
+scroll_button = HoverButton(ventana, text= "SCROLL ONE PAGE", height=2, width=18,font= ('Bahnschrift',13), borderwidth=2,  relief = "raised", activebackground="dark goldenrod", bg='gold',  anchor="center",command=lambda: scroll())  
+scroll_button.grid(column =1, row=1, pady=4,padx=7)
 
-start = HoverButton(frame, text= "Start Again", height=2, width=18,font= ('Bahnschrift',13), borderwidth=2,  relief = "raised", activebackground="dark goldenrod", bg='gold',  anchor="center",command=lambda: limpiar())  
-start.grid(column =3, row=2, pady=2,padx=2)
+rollback_button = HoverButton(ventana, text= "Rollback one page", height=2, width=18,font= ('Bahnschrift',13), borderwidth=2,  relief = "raised", activebackground="dark goldenrod", bg='gold',  anchor="center",command=lambda: rollback())  
+rollback_button.grid(column =1, row=2, pady=4,padx=7)
 
-Resultado = Entry(frame, bg='#9EF8E8', width=18, relief='groove', font='Monstserrat 16', justif='right')
-Resultado.grid(columnspan=4 , row=0, pady=3,padx=1, ipadx=1, ipady=1)
+reset_button = HoverButton(ventana, text= "Start Again", height=2, width=18,font= ('Bahnschrift',13), borderwidth=2,  relief = "raised", activebackground="dark goldenrod", bg='gold',  anchor="center",command=lambda: reset())  
+reset_button.grid(column =1, row=3, pady=4,padx=7)
+
+
+Resultado = Entry(ventana, bg='gray', width=10, relief='groove', font =('Bahnschrift',20), justif='center')
+Resultado.grid(columnspan=10 , row=4, pady=3,padx=1, ipadx=1, ipady=1)
 
 ventana.mainloop()
 
